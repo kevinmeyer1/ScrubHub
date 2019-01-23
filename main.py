644984abb2e6aeb707cd79fb9f8984992c2f0ff0
Kevin Meyer <kpm216@gmail.com>
2019-01-22 19:53:07 -0500

    bare bones functionality done

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
 
 app = Flask(__name__)
@@ -10,34 +10,61 @@ mysql_config = {
     'host': 'localhost',
     'database': 'SubHub'}
 
+signed_in = False
+
 @app.route('/')
 def home():
     return render_template('signin.html')
 
-@app.route('/handle_signin',  methods=['POST'])
+@app.route('/handle_signin',  methods=['POST', 'GET'])
 def handle_signin():
-    username = request.form['signin_username']
-    password = request.form['signin_password']
+    global signed_in
+    if request.method == 'POST':
+        username = request.form['signin_username']
+        password = request.form['signin_password']
 
-    conn = mysql.connector.connect(**mysql_config)
-    cur = conn.cursor()
-    result = cur.execute("SELECT * FROM users WHERE username=\"{}\" AND password=\"{}\";".format(username, password))
+        conn = mysql.connector.connect(**mysql_config)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM users WHERE username=\"{}\" AND password=\"{}\";".format(username, password))
 
-    if cur.fetchone():
-        return render_template('success.html')
+        if cur.fetchone():
+            signed_in = True
+            return redirect(url_for('user_data', username=username))
+        else:
+            return render_template('signin.html', error_message="Incorrect login credentials.")
     else:
-        return render_template('signin.html', error_message="Incorrect login credentials.")
+        return render_template('signin.html')
 
 @app.route('/handle_signup', methods=['POST', 'GET'])
 def handle_signup():
-    username = request.form['signup_username']
-    password = request.form['signup_password']
+    if request.method == 'POST':
+        #method is post, data is recieved
+        username = request.form['signup_username']
+        password = request.form['signup_password']
 
-    #add entry to users table
-    #create table named what the username is
-    return ""
+        #Add new user to users table
+        conn = mysql.connector.connect(**mysql_config)
+        cur = conn.cursor()
+        cur.execute("INSERT INTO users (username, password) VALUES (\"{}\", \"{}\");".format(username, password))
+        conn.commit()
 
+        # TODO: create table named what the username is, better way to do this?
 
+        #send user back to signin page
+        return render_template('signin.html', account_creation="Account successfully created.")
+    else:
+        #signup page is served when getting handle_signup
+        return render_template('signup.html')
 
-# winpty mysql -u Kevin -p
-# to login to mysql from windows git bash
+@app.route('/user/<username>', methods=['POST', 'GET'])
+def user_data(username):
+    if signed_in == False:
+        return redirect(url_for('handle_signin'))
+
+    conn = mysql.connector.connect(**mysql_config)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM {};".format(username))
+    data = cur.fetchall()
+
+    #sends all user data to the user.html page
+    return render_template('user.html', data=data)
