dc2baa454475ec9422ef85fd8baf4e99108f2494
Kevin Meyer <kpm216@gmail.com>
2019-04-05 05:47:52 -0400

    demo ready

--- main.py
+++ main.py
@@ -3,12 +3,6 @@ import mysql.connector, ssl, smtplib, schedule, time, sys
 from datetime import datetime
 from twilio.rest import Client
 
-
-######madj had to change it to mysql-connector-python for msq connector to work#####
-
-###### instead of check_for_reminders, I am going to have a test button that allows users to get an email or text vs
-###### having to wait for one to show up, better for demos and presentations.
-
 app = Flask(__name__)
 app.secret_key = 'some secret key?' #This is needed to make my session work. what do i do for this?
 
@@ -24,47 +18,28 @@ mysql_config = {
 twilio_sid = 'ACd28df43aa8b7a9d5ffe1fb74d98be3a6'
 twilio_auth_token = '7188d25025fbef6d7838eed5dc1a9e4f'
 
-#---Working----- fix subject
-#send_email("Chegg", 'Kevin', 'Meyer', '3/25', 'kpm216@gmail.com')
-
-#----Working----
-#send_text("9194515213", 'Chegg', '3/25')
-
 #Home page, shows up when you go to localhost:5000
 @app.route('/')
 def home():
     session['logged_in'] = False
     return render_template('frontPage.html')
 
-##### Here we will keep our list of subscription types
-def get_sub_type(name):
-    type = ""
-    educational = ['chegg', 'zybooks']
-    entertainment = ['netflix', 'hulu', 'amazon']
-
-    if name in educational:
-        type = 'Education'
-    elif name in entertainment:
-        type = 'Entertainment'
-    else:
-        type = 'Other'
-
-    return type
-
 #signs in user when they exist in the users table -- may change in the future with database
-@app.route('/handle_signin',  methods=['POST', 'GET'])
-def handle_signin():
+@app.route('/Sign_In',  methods=['POST', 'GET'])
+def Sign_In():
     if request.method == 'POST':
-        username = request.form['signin_username']
-        password = request.form['signin_password']
+        email = request.form['email']
+        password = request.form['password']
 
         conn = mysql.connector.connect(**mysql_config)
         cur = conn.cursor()
-        cur.execute(f"SELECT username, password FROM users WHERE username='{username}' AND password='{password}';")
+        cur.execute(f"SELECT email, name, password FROM users WHERE email='{email}' AND password='{password}';")
 
         if cur.fetchone():
-            session['username'] = username
+            session['email'] = email
             session['logged_in'] = True
+            cur.execute(f"SELECT name FROM users WHERE email='{email}'")
+            session['name'] = cur.fetchone()[0]
             return redirect(url_for('user_data'))
         else:
             return render_template('login.html', error_message="Incorrect login credentials.")
@@ -72,8 +47,8 @@ def handle_signin():
         return render_template('login.html')
 
 #handle signup and creates a new user table -- may change in the future with database
-@app.route('/handle_signup', methods=['POST', 'GET'])
-def handle_signup():
+@app.route('/Sign_Up', methods=['POST', 'GET'])
+def Sign_Up():
     if request.method == 'POST':
         #method is post, data is recieved
         form_username = request.form['username']
@@ -90,64 +65,121 @@ def handle_signup():
         conn.commit()
 
         #send user back to signin page
-        return render_template('frontPage.html', account_creation="Account successfully created.")
+        return render_template('frontPage.html')
     else:
-        #signup page is served when getting handle_signup
+        #signup page is served when getting Sign_Up
         return render_template('createAccount.html')
+
+#saves personal information in signup sequence
+@app.route('/notification_method', methods=['POST'])
+def notification_method():
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+    phone_number = request.form['phone_number']
+
+    session['personal'] = [name, email, phone_number, password]
+
+    return render_template('notificationMethod.html')
+
+#saves notif method in signup sequence
+@app.route('/select_subscriptions', methods=['POST'])
+def select_subscriptions():
+    notification_method = request.form['notification_method']
+    session['notification_method'] = notification_method
+    return render_template('manageSubscription.html')
 
 #grabs all the subscriptions for a user - may change in the future with database
 @app.route('/user', methods=['POST', 'GET'])
 def user_data():
-    if session['logged_in'] == False:
-        return redirect(url_for('handle_signin'))
-
     conn = mysql.connector.connect(**mysql_config)
     cur = conn.cursor()
-    cur.execute(f"SELECT * FROM subscription where USERNAME='{session['username']}';")
+
+    if request.method == 'POST':
+        session['logged_in'] = True
+        session['email'] = session['personal'][1]
+        session['name'] = session['personal'][0]
+        cur.execute(f"INSERT INTO users VALUES ('{session['personal'][0]}', '{session['personal'][1]}', '{session['personal'][2]}', '{session['notification_method']}', '{session['personal'][3]}')")
+        conn.commit()
+        session.pop('personal')
+        session.pop('notification_method')
+
+    if session['logged_in'] == False:
+        return redirect(url_for('Sign_In'))
+
+    cur.execute(f"SELECT * FROM subscription where email='{session['email']}';")
     data = cur.fetchall()
 
     #sends all user data to the user.html page
-    return render_template('user.html', data=data, username=session['username'])
+    return render_template('home.html', data=data, email=session['email'], name=session['name'])
 
-#adds a subscription - will change in future with database - extra feilds and stuff
+#adds a subscription - simplified for now
 @app.route('/add_subscription', methods=['POST', 'GET'])
 def add_subscription():
     if session['logged_in'] == False:
-        return redirect(url_for('handle_signin'))
+        return redirect(url_for('Sign_In'))
 
     if request.method == 'POST':
         sub_name = request.form['sub_name']
         sub_price = request.form['sub_price']
-        sub_purchase_date = request.form['sub_purchase_date']
         sub_renewal_date = request.form['sub_renewal_date']
         notification_type = request.form['notification_type']
-        subscription_type = get_sub_type(sub_name);
+        subscription_type = request.form['sub_type']
         active_sub = 1
-
-        ######
-        # Need to do a bunch of error handling here at some point - dont use Date for sql or just fuck with the formatting
-        ######
 
         conn = mysql.connector.connect(**mysql_config)
         cur = conn.cursor()
-        cur.execute(f"INSERT INTO subscription ('{session['username']}', '{sub_name}', {sub_price}, {sub_purchase_date}, {sub_renewal_date}, '{notification_type}', '{subscription_type}', {active_sub});")
+        cur.execute(f"INSERT INTO subscription VALUES ('{session['email']}', '{sub_name}', {sub_price}, {sub_renewal_date}, '{notification_type}', '{subscription_type}', 1);")
         conn.commit()
 
-        return redirect(url_for('user_data', username=session['username']))
+        return redirect(url_for('user_data', email=session['email']))
     else:
-        return render_template('add_subscription.html', username=session['username'])
+        return render_template('newSubscription.html', email=session['email'])
 
-#log out function, logs them out and sends them back to signing
+#log out function, logs them out and sends them back to signin page. removes identification from session
 @app.route('/logout')
 def logout():
-    session.pop('username')
+    session.pop('email')
+    session.pop('name')
     session['logged_in'] = False
-    return redirect(url_for('handle_signin'))
+    return redirect(url_for('home'))
+
+#takes user to manage a single subscription
+@app.route('/manage_subscription')
+def manage_subscription():
+
+    #code to be added in the future
+
+    return render_template('homeManage.html', name=session['name'])
+
+#response from confirm/renew button in manage page
+@app.route('/confirm_renewal')
+def renew_subscription():
+
+    #Code to be added in the future
+
+    return render_template('homeConfirm.html')
 
 #this is the set 404 page so when a user tries to go somewhere that doesnt exist we can just send them this
-#@app.errorhandler(404)
-#def page_not_found(e):
-    #return render_template('404.html')
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('old/404.html')
+
+#function to demonstrate text capabilites
+@app.route('/test_text')
+def test_text():
+    conn = mysql.connector.connect(**mysql_config)
+    cur = conn.cursor()
+    cur.execute(f"select phone_number from users where email='{session['email']}'")
+    phone_number = cur.fetchone()[0]
+    send_text(phone_number, "[Subscription Placeholder]", "[Date Placeholder]")
+    return redirect(url_for('user_data'))
+
+#function to demonstrate email capabilites
+@app.route('/test_email')
+def test_email():
+    send_email("[Subscription Placerholder]", session['name'], '[Date Placeholder]', session['email'])
+    return redirect(url_for('user_data'))
 
 #this will send a text when called - not actually used in site yet
 def send_text(phone_number, sub_name, sub_renewal_date):
@@ -160,7 +192,7 @@ def send_text(phone_number, sub_name, sub_renewal_date):
     return
 
 #this will send an email when called - not actually used in site yet
-def send_email(sub_name, first_name, last_name, sub_renewal_date, email_address):
+def send_email(sub_name, name, sub_renewal_date, email_address):
     port = 465 #i can probably change this who knows
     password = 'kPm130806!' #password for scrubhubreminders@gmail.com
     smtp_server = "smtp.gmail.com" #this is needed and doesnt change
@@ -170,7 +202,7 @@ def send_email(sub_name, first_name, last_name, sub_renewal_date, email_address)
     \n
     Subject: Reminder - Your {sub_name} subscription is renewing soon!
     \n
-    Dear {first_name} {last_name},
+    Dear {name},
 
     Your subscription {sub_name} is set to renew on {sub_renewal_date}.
 
@@ -181,6 +213,26 @@ def send_email(sub_name, first_name, last_name, sub_renewal_date, email_address)
         server.login(sender_email, password)
         server.sendmail(sender_email, email_address, message)
         server.quit()
+
+
+#-------------------------------------------------------------------------------------------------
+#           UNUSED FUNCTIONS BELOW - POSSIBLE IMPLEMENTATION OPTIONS
+#-------------------------------------------------------------------------------------------------
+
+##### Here we will keep our list of subscription types - eh maybe not, that might be done within the html
+def get_sub_type(name):
+    type = ""
+    educational = ['chegg', 'zybooks'] # lists imcomplete - probably a bad idea
+    entertainment = ['netflix', 'hulu', 'amazon']
+
+    if name in educational:
+        type = 'Education'
+    elif name in entertainment:
+        type = 'Entertainment'
+    else:
+        type = 'Other'
+
+    return type
 
 # here we check each day if users in our database have subscriptions that are renewing soon, if so we send them an email or text
 def check_for_reminders():
@@ -209,11 +261,11 @@ def get_user_profile():
     conn = mysql.connector.connect(**mysql_config)
     cur = conn.cursor()
 
-    cur.execute(f"SELECT * FROM users WHERE username='{session['userame']}';")
+    cur.execute(f"SELECT * FROM users WHERE email='{session['email']}';")
     user_data = cur.fetchall()
     #this is all of the users personal information
 
-    cur.execute(f"SELECT * FROM subscription WHERE username='{session['username']}';")
+    cur.execute(f"SELECT * FROM subscription WHERE email='{session['email']}';")
     sub_data = cur.fetchall()
 
     for row in sub_data:
@@ -229,7 +281,7 @@ def get_user_subscriptions():
     conn = mysql.connector.connect(**mysql_config)
     cur = conn.cursor()
 
-    cur.execute(f"SELECT sub_name FROM subscription where username='{session['username']}';")
+    cur.execute(f"SELECT sub_name FROM subscription where email='{session['email']}';")
     sub_data = cur.fetchall()
 
     return sub_data
